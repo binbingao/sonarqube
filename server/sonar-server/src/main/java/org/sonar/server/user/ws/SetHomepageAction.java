e68490ad2e54dc7eca9a2fb25efd9d549f7c3160
Guillaume Jambet <guillaume.jambet@sonarsource.com>
2017-12-14 12:04:25 +0100

    SONAR-10182 Add api/users/set_homepage persistence

--- server/sonar-server/src/main/java/org/sonar/server/user/ws/SetHomepageAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/user/ws/SetHomepageAction.java
@@ -22,13 +22,35 @@ package org.sonar.server.user.ws;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
 import org.sonar.api.server.ws.WebService;
+import org.sonar.db.DbClient;
+import org.sonar.db.DbSession;
+import org.sonar.db.user.UserDto;
+import org.sonar.server.component.ComponentFinder;
+import org.sonar.server.user.UserSession;
+
+import static com.google.common.base.Preconditions.checkState;
+import static org.apache.commons.lang.StringUtils.isBlank;
+import static org.sonar.server.ws.WsUtils.checkFoundWithOptional;
+import static org.sonarqube.ws.Users.CurrentWsResponse.HomepageType.MY_ISSUES;
+import static org.sonarqube.ws.Users.CurrentWsResponse.HomepageType.MY_PROJECTS;
+import static org.sonarqube.ws.Users.CurrentWsResponse.HomepageType.ORGANIZATION;
+import static org.sonarqube.ws.Users.CurrentWsResponse.HomepageType.PROJECT;
 
 public class SetHomepageAction implements UsersWsAction {
 
-  private static final String PARAM_TYPE = "type";
-  private static final String PARAM_VALUE = "value";
-  private static final String ACTION = "set_homepage";
+  static final String PARAM_TYPE = "type";
+  static final String PARAM_VALUE = "value";
+  static final String ACTION = "set_homepage";
 
+  private final UserSession userSession;
+  private final DbClient dbClient;
+  private final ComponentFinder componentFinder;
+
+  public SetHomepageAction(UserSession userSession, DbClient dbClient, ComponentFinder componentFinder) {
+    this.userSession = userSession;
+    this.dbClient = dbClient;
+    this.componentFinder = componentFinder;
+  }
 
   @Override
   public void define(WebService.NewController controller) {
@@ -51,9 +73,47 @@ public class SetHomepageAction implements UsersWsAction {
 
   @Override
   public void handle(Request request, Response response) throws Exception {
+    userSession.checkLoggedIn();
 
-    // WIP : Not implemented yet.
+    String type = request.mandatoryParam(PARAM_TYPE);
+    String value = request.param(PARAM_VALUE);
+
+    if (PROJECT.toString().equals(type) && isBlank(value)) {
+      throw new IllegalArgumentException("type PROJECT requires a mandatory project key");
+    }
+
+    if (ORGANIZATION.toString().equals(type) && isBlank(value)) {
+      throw new IllegalArgumentException("type ORGANIZATION requires a mandatory project key");
+    }
+
+    String userLogin = userSession.getLogin();
+
+    String uuid = null;
+
+    try (DbSession dbSession = dbClient.openSession(false)) {
+
+      UserDto userDto = dbClient.userDao().selectActiveUserByLogin(dbSession, userLogin);
+      checkState(userDto != null, "User login '%s' cannot be found", userLogin);
+
+      if (PROJECT.toString().equals(type)) {
+        uuid = componentFinder.getByKey(dbSession, value).uuid();
+      }
+
+      if (ORGANIZATION.toString().equals(type)) {
+        uuid = checkFoundWithOptional(dbClient.organizationDao().selectByKey(dbSession, value), "No organizationDto with key '%s'", value).getUuid();
+      }
+
+      if ((MY_PROJECTS.toString().equals(type) || MY_ISSUES.toString().equals(type)) && value != null) {
+        throw new IllegalArgumentException("value parameter must be null when type = [MY_PROJECTS | MY_ISSUES]");
+      }
+
+      userDto.setHomepageType(type);
+      userDto.setHomepageValue(uuid);
+
+      dbClient.userDao().update(dbSession, userDto);
+      dbSession.commit();
+    }
+
     response.noContent();
-
   }
 }
